8edb8af453b99f73a7f1b45773d7fe6cc990ba2e
Mohit <mohit3312@gmail.com>
2022-07-26 11:36:05 +0530

    11th video is completed

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -18,6 +18,17 @@ export default function TextForm(props) {
     setText(newText);
   };
 
+  const handleCopy = () => {
+    var text = document.getElementById("myBox");
+    text.select();
+    navigator.clipboard.writeText(text.value);
+  };
+
+  const handleExtraSpaces = () => {
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(" "));
+  };
+
   const handleOnChange = (event) => {
     // console.log("On change");
     setText(event.target.value);
@@ -50,10 +61,18 @@ export default function TextForm(props) {
         <button className="btn btn-primary mx-1" onClick={handleClearClick}>
           Clear Text
         </button>
+        <button className="btn btn-primary mx-1" onClick={handleCopy}>
+          Copy Text
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handleExtraSpaces}>
+          Remove Extra Spaces
+        </button>
       </div>
       <div className="container my-3">
         <h2>Your text summary</h2>
-        <p>{text.split(" ").length} words and {text.length} characters</p>
+        <p>
+          {text.split(" ").length} words and {text.length} characters
+        </p>
         <p>{0.008 * text.split(" ").length} Minutes read</p>
         <h2>Preview</h2>
         <p>{text}</p>
